f4e8de7b485a1abe3bf964d0f9a57b3b7d521454
Aftab mohammed k <87110394+aftabmk@users.noreply.github.com>
2022-09-08 09:05:49 +0530

    Update Navbar.js

--- components/Navbar.js
+++ components/Navbar.js
@@ -2,7 +2,7 @@ import Link from "next/link";
 import {AiOutlineShopping} from 'react-icons/ai'
 import { useStateContext } from "../Context/Context";
 import Cart from "./Cart";
-import png from "../public/ecommerce.png"
+import png from "../../public/ecommerce.png"
 
 const Navbar = () => {
     const { showCart , setShowCart , totalQuantities } = useStateContext()
