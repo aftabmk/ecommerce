eaf7b99a7ceb43704ab538927ef34f35c1e653aa
Aftab mohammed k <87110394+aftabmk@users.noreply.github.com>
2022-09-08 14:00:07 +0530

    Update Navbar.js

--- components/Navbar.js
+++ components/Navbar.js
@@ -2,12 +2,7 @@ import Link from "next/link";
 import { AiOutlineShopping } from 'react-icons/ai'
 import { useStateContext } from "../Context/Context";
 import Cart from "./Cart";
-<<<<<<< HEAD
-// use Image property in Nextjs
 import Image from "next/image";
-=======
-import png from "../public/ecommerce.png"
->>>>>>> 047987a3d47bd6c372a9d7b8017de083aafb2fa8
 
 const Navbar = () => {
     const { showCart, setShowCart, totalQuantities } = useStateContext()
@@ -22,14 +17,7 @@ const Navbar = () => {
                 />
             </Link>
             <p>
-<<<<<<< HEAD
-                <Link href='/'>
-                    <h2>Ecommerce</h2>
-                </Link>
-=======
-                <img src={png} href="/" />
                 <Link href='/'>Ecommerce</Link>
->>>>>>> 047987a3d47bd6c372a9d7b8017de083aafb2fa8
             </p>
             <button type='button' className="cart-icon" onClick={() => setShowCart(true)}>
                 <AiOutlineShopping />
@@ -41,10 +29,5 @@ const Navbar = () => {
         </div>
     );
 }
-<<<<<<< HEAD
+export default Navbar;
 
-export default Navbar;
-=======
- 
-export default Navbar;
->>>>>>> 047987a3d47bd6c372a9d7b8017de083aafb2fa8
